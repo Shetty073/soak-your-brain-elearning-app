11fb28082d28b29288c915b79c4970cb1efc81f3
Alpha Sierra <30448930+Shetty073@users.noreply.github.com>
2021-01-06 02:59:49 +0530

    teacher post filtering done

--- app/static/js/teacher_create_post.js
+++ app/static/js/teacher_create_post.js
@@ -300,3 +300,23 @@ $('.post-delete-btn').on('click', function () {
         }
     });
 });
+
+// Posts filter
+$('#subjectfilter').on('change', function () {
+    let class_name = $(this).val();
+    let posts = $('.posts');
+
+    for (let i = 0; i < posts.length; i++) {
+        if (posts[i].classList.contains('hidepost')) {
+            posts[i].classList.remove('hidepost');
+        }
+    }
+
+    if (class_name !== 'all') {
+        for (let i = 0; i < posts.length; i++) {
+            if (!posts[i].classList.contains('hidepost') && !posts[i].classList.contains(class_name)) {
+                posts[i].classList.add('hidepost');
+            }
+        }
+    }
+})
