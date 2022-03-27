5baac91783a506ad6e4823e484ee14b72d8250f2
Ashish Shetty <shetty073@gmail.com>
2022-03-27 17:33:06 +0530

    fix static

--- app/app/settings.py
+++ app/app/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 # Media root gives django location to save product images
 MEDIA_URL = '/media/'
